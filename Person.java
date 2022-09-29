e7ad211891d333faedd3acdf36d24678aadaec43
Buzymom <fiomarelgai@yahoo.com>
2022-09-29 11:50:42 +0100

    Initial commit

---
+++ Person.java
@@ -0,0 +1,44 @@
+public class Person {
+   private String name;
+   private int age;
+   private double height;
+   private double weight;
+
+    public Person (String name, int age, double height, double weight){
+        setName(name);
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
+    }
+    public String getName(){
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = Name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int Age) {
+        this.age = Age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double Height) {
+        this.height = Height;
+    }
+    public double setWeight(){
+        return weight;
+
+    }
+    public void setWeight(double Weight){
+        this.weight = Weight;
+    }
+}
---
+++ PersonCreator1.java
@@ -0,0 +1,8 @@
+public class PersonCreator1 {
+    public static void main(String[] args) {
+        Person Person01 = new Person("James Peterson" ,34,180,73.5);
+        Person Person02 = new Person("Bob Thornley",25,171,68.2);
+        System.out.println(Person01.getName()+Person01.getAge()+Person01.getHeight()+Person01.getWeight());
+        System.out.println(Person02.getName()+Person02.getAge()+Person02.getHeight()+Person02.getWeight());
+    }
+}
